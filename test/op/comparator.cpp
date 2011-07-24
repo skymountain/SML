977101cc7f4797c7b7cf1204a617e60de955d7f8
Taro Sekiyama <t-sekiym@kuis.kyoto-u.ac.jp>
2011-07-24 17:37:54 +0900

    add test for comparator/lesser

---
+++ test/op/comparator.cpp
@@ -0,0 +1,43 @@
+#include <gtest/gtest.h>
+#include "sml/op/comparator.hpp"
+
+namespace {
+
+class Comparator : public testing::Test {
+protected:
+
+  void SetUp() {
+    for (int i = 0; i < Comparator::SIZE; ++i) {
+      this->arr_[i] = i;
+    }
+  }
+
+  static const int SIZE = 3;
+  int arr_[SIZE];
+  sml::op::comparator cmp_;
+};
+
+TEST_F(Comparator, less) {
+  for (int i = 0; i < Comparator::SIZE - 1; ++i) {
+    ASSERT_EQ(-1, this->cmp_(this->arr_[i], this->arr_[i+1]));
+  }
+}
+
+TEST_F(Comparator, greater) {
+  for (int i = 0; i < Comparator::SIZE - 1; ++i) {
+    ASSERT_EQ(1, this->cmp_(this->arr_[i+1], this->arr_[i]));
+  }
+}
+
+TEST_F(Comparator, equal) {
+  for (int i = 0; i < Comparator::SIZE; ++i) {
+    ASSERT_EQ(0, this->cmp_(this->arr_[i], this->arr_[i]));
+  }
+}
+
+} // namespace
+
+int main(int argc, char** argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
---
+++ test/op/lesser.cpp
@@ -0,0 +1,43 @@
+#include <gtest/gtest.h>
+#include "sml/op/lesser.hpp"
+
+namespace {
+
+class Lesser : public testing::Test {
+protected:
+
+  void SetUp() {
+    for (int i = 0; i < Lesser::SIZE; ++i) {
+      this->arr_[i] = i;
+    }
+  }
+
+  static const int SIZE = 3;
+  int arr_[SIZE];
+  sml::op::lesser lesser_;
+};
+
+TEST_F(Lesser, less) {
+  for (int i = 0; i < Lesser::SIZE - 1; ++i) {
+    ASSERT_TRUE(this->lesser_(this->arr_[i], this->arr_[i+1]));
+  }
+}
+
+TEST_F(Lesser, greater) {
+  for (int i = 0; i < Lesser::SIZE - 1; ++i) {
+    ASSERT_FALSE(this->lesser_(this->arr_[i+1], this->arr_[i]));
+  }
+}
+
+TEST_F(Lesser, equal) {
+  for (int i = 0; i < Lesser::SIZE; ++i) {
+    ASSERT_FALSE(this->lesser_(this->arr_[i], this->arr_[i]));
+  }
+}
+
+} // namespace
+
+int main(int argc, char** argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
